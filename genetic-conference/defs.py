e6c490b9e6ca317f3631b2d03a39df9510f6b6cf
joao-conde <joaoconde_13@hotmail.com>
2018-03-09 13:22:38 +0000

    selection and match done, next: cross-over and mutate

--- genetic-conference/defs.py
+++ genetic-conference/defs.py
@@ -1,7 +1,5 @@
 from objects import *
 
-#functions and "constants" here
-
 NUMBER_OF_CROMOSSOMES = 6
 CROMOSSOME_SIZE = 10
 
@@ -15,6 +13,7 @@ def init_random_population():
     
     return population
 
+#calculate selection probabilities for each individual
 def selection_probabilities(population):
     selectP = []
     totalFitness = 0
@@ -26,22 +25,40 @@ def selection_probabilities(population):
 
     return selectP
 
-def create_selection_piechart(selectProbs):
+#create a 'pie chart' of probability for each individual to be chosen for matching
+def create_selection_piechart(select_probs):
     intervals = []
     lowerB = 0
-    for prob in selectProbs:
+    for prob in select_probs:
         interval = Interval(lowerB, lowerB + prob)
         intervals.append(interval)
         lowerB += prob
     return intervals
 
-def select_for_match(population, selectionPieChart):
-    popIndexes = []
+#select individuals for matching based on the pie chart
+def select_for_match(population, selection_piechart):
+    pop_idx = []
     for i in range(NUMBER_OF_CROMOSSOMES):
         rand = random.uniform(0,1)
-        for index in range(len(selectionPieChart)):
-            if(selectionPieChart[index].contains(rand)) : 
-                popIndexes.append(index)
+        for index in range(len(selection_piechart)):
+            if(selection_piechart[index].contains(rand)) : 
+                pop_idx.append(index)
                 break
                 
-    return popIndexes
+    return pop_idx
+
+#matches individuals for cross-over
+def match(selected_for_matching):
+    matches = []
+
+    for i in range(NUMBER_OF_CROMOSSOMES):
+        rand1 = rand2 = 0
+        
+        while rand1 == rand2:
+            rand1 = random.randint(0, len(selected_for_matching)-1)
+            rand2 = random.randint(0, len(selected_for_matching)-1)
+        
+        match = [selected_for_matching[rand1], selected_for_matching[rand2]]
+        matches.append(match)
+
+    return matches
--- genetic-conference/scheduler.py
+++ genetic-conference/scheduler.py
@@ -6,24 +6,41 @@ random.seed()
 
 #step1 - initial population
 population = init_random_population()
+#introduce perfect genome to see population rapidly go towards it and change number of cycles to ~10
+#schedule = Schedule()
+#schedule.DNA = [1,1,1,1,1,1,1,1,1,1]
+#population[0] = schedule
 
 
-#print of population
-for schedule in population:
-    schedule.print()
-
-
-#select match cross mutate terminate?
 i = 0
 while(i != 1):
-    #step2 - selection for matching
+
+    #step2 - select for matching
     selection_prob = selection_probabilities(population)
     select_piechart = create_selection_piechart(selection_prob)
-    selectedForMatching = select_for_match(population, select_piechart)
-
-    print(selectedForMatching)
+    selected_for_matching = select_for_match(population, select_piechart)
 
 
+    #step3 - match
+    matches = match(selected_for_matching)
+    print("Matches:", matches)
+
+    #step4 - cross-over
+
+    #step5 - mutations
+
+    #step6 - (terminate?)
+
+    #print of population
+    for schedule in population:
+        schedule.print()
+    print(selected_for_matching,'\n\n')
+
+    newpop = []
+    for idx in selected_for_matching:
+        newpop.append(population[idx])
+
+    
     #Criar intervalos, gerar random numbers NUMBER_OF_CROMOSSOMES times, emparelhar esses, cruzal-los, mutá-los 
     i+=1
 
